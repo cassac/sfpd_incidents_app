46bd82b9a6b24948fb2104d5500a10c3e6dbe5ce
cassac <hicassac@gmail.com>
2016-04-12 16:24:06 -0700

    adds two functions to dynamically create filter select menus

--- sfpd.js
+++ sfpd.js
@@ -1,10 +1,14 @@
-var BASE_URL = 'https://data.sfgov.org/resource/cuks-n6tp.json?'
+var BASE_URL = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=5"; // this endpoint newer than `tmnf-yvry.json`
+/*
+Filter date example: BASE_URL + $where=date between '2014-01-10T12:00:00' and '2015-01-10T14:00:00'
+*/
 
 function loadData(url, f) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var data = JSON.parse(xhttp.responseText);
+			console.log('data: ', data);
 			f(data);
 		}
 	};
@@ -13,4 +17,18 @@ function loadData(url, f) {
 
 };
 
-loadData(BASE_URL, function(stuff){ console.log(stuff); });
+function makeMenus(param) {
+	var menu = '<select id="' + param + '"><option value="--">' + param + '</option></select>';
+	var filters = window.document.getElementById('filters');
+	filters.innerHTML += menu;
+}
+
+function populateMenus(array) {
+	params = Object.keys(array[0]);
+	params.map(makeMenus);
+	var category = window.document.getElementById('location');
+	console.log(category);
+};
+
+
+loadData(BASE_URL, populateMenus);
