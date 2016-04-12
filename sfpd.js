2e3fc2935d17914e58b8386f3fc1b357662c926f
cassac <hicassac@gmail.com>
2016-04-12 14:44:20 -0700

    uses callback function for ajax

--- sfpd.js
+++ sfpd.js
@@ -1,18 +1,16 @@
-(function(){
+var BASE_URL = 'https://data.sfgov.org/resource/cuks-n6tp.json?'
 
-	var url = 'https://data.sfgov.org/resource/cuks-n6tp.json?';
+function loadData(url, f) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var data = JSON.parse(xhttp.responseText);
-			return presentData(data);
+			f(data);
 		}
 	};
 	xhttp.open('GET', url, true);
 	xhttp.send();
 
-})();
+};
 
-function presentData(data) {
-	console.log(data.length);
-}
+loadData(BASE_URL, function(stuff){ console.log(stuff); });
