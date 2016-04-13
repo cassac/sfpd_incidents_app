b1de50d821aca97295665a12c6da4593b1cba7a8
cassac <hicassac@gmail.com>
2016-04-12 22:40:39 -0700

    adds incident object helper construction function, adds method to Incident object

--- sfpd.js
+++ sfpd.js
@@ -3,13 +3,47 @@ var BASE_URL = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=5"; // thi
 Filter date example: BASE_URL + $where=date between '2014-01-10T12:00:00' and '2015-01-10T14:00:00'
 */
 
+function Incident(address, category, date, dayofweek, descript,
+					incidntnum, location, pddistrict, pdid,
+					resolution, time, x, y) {
+
+	this.address = address;
+	this.category = category;
+	this.date = date;
+	this.dayofweek = dayofweek;
+	this.descript = descript;
+	this.incidntnum = incidntnum;
+	this.location = location;
+	this.pddistrict = pddistrict;
+	this.pdid = pdid;
+	this.resolution = resolution;
+	this.time = time;
+	this.x = x;
+	this.y = y;
+
+}
+
+Incident.prototype.report = function(incident) {
+	console.log( 'On', this.date, this.dayofweek, 'at', this.time,'a\nin the',
+		this.pddistrict, 'there was a(n)', this.category,'incident.')
+}
+
+function getIncidentObj(incident) {
+	var newIncident = new Incident();
+	for (var item in incident) {
+		newIncident[item] = incident[item];
+	}
+	return newIncident;
+}
+
 function loadData(url, f) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var data = JSON.parse(xhttp.responseText);
-			console.log('data: ', data);
 			f(data);
+			var hey = data.map(function(incident) { return getIncidentObj(incident); });
+			hey[0].report();
 		}
 	};
 	xhttp.open('GET', url, true);
