97cb9d5b9752271a3f78e825b0bf4ba649e69c3a
cassac <hicassac@gmail.com>
2016-04-13 17:31:06 -0700

    seperates form submit btn event handler and called function logic. adds url construction for strings, but not dates and times yet

--- sfpd.js
+++ sfpd.js
@@ -1,6 +1,8 @@
-var BASE_URL = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=5"; // this endpoint newer than `tmnf-yvry.json`
+var BASE_URL = "https://data.sfgov.org/resource/cuks-n6tp.json?"; // this endpoint newer than `tmnf-yvry.json`
 /*
 Filter date example: BASE_URL + $where=date between '2014-01-10T12:00:00' and '2015-01-10T14:00:00'
+
+https://data.sfgov.org/resource/cuks-n6tp.json?$where=category IN ('ASSAULT', 'KIDNAPPING') AND dayofweek IN ('Monday', 'Saturday') AND date between '2014-01-10T12:00:00' and '2015-01-10T14:00:00' 
 */
 
 function Incident(address, category, date, dayofweek, descript,
@@ -99,23 +101,34 @@ var filterFormSubmitBtn = window.document.getElementById('filterSubmit');
 filterFormSubmitBtn.addEventListener('click', function(e) { 
 
 	e.preventDefault();
-	
-	var form = window.document.getElementById('filterForm');
+	parseForm();
+
+});
+
+function parseForm() {
+
+	var urlLimitParam;
 	var urlParameters = [];
+	var form = window.document.getElementById('filterForm');
 
 	for (var i = 0; i < form.elements.length - 1; i++) {
 
 		var el = form.elements[i];
+		// console.log(el.name, el.name=='limit');
 		var urlParam = {};
 		urlParam[el.name] = [];
 
-		if (typeof el.selectedOptions != 'undefined' && el.selectedOptions.length > 0) {
+		if (el.name == 'limit') {
+			urlLimitParam = el.value;
+		} 
+		else if (typeof el.selectedOptions != 'undefined' && el.selectedOptions.length > 0) {
 			var options = el.selectedOptions;
 			urlParam[el.name] = [];
 			for (var j = 0; j < options.length; j++) {
 				urlParam[el.name].push(options[j].value);
 			}
-		} else if (el.value) {
+		} 
+		else if (el.value) {
 			urlParam[el.name].push(form.elements[i].value);
 		}
 
@@ -124,12 +137,64 @@ filterFormSubmitBtn.addEventListener('click', function(e) {
 		}
 
 	}
+
+	constructUrl(urlParameters, urlLimitParam);
+}
+
+function formatUrlDateAndTimeParameters(type, array) {
+	return type + " between '" + array[0] + "'' and '" + array[1] +"'";
+}
+
+function formatUrlStringParameters(type, array) {
+	return type + " IN ('" + array.join("', '") + "')";
+}
+
+function formatUrlParameters(array) {
+
+	var urlTail = '$where='
 	
-	console.log('urlParameters', urlParameters);
+	for (var i in array) {
 
-});
+		var obj = array[i];
+		var urlParam = Object.keys(obj)[0];
+		var urlParamArgs = obj[urlParam];
+		var checkIfDateOrTime = urlParam.slice(0,4);	
+
+		if (checkIfDateOrTime == 'date' || checkIfDateOrTime == 'time') {
+
+			var urlExt = formatUrlDateAndTimeParameters(urlParam, urlParamArgs);
+
+		} else {
+			var urlExt = formatUrlStringParameters(urlParam, urlParamArgs);
+
+		}
+
+		urlTail += urlExt;
+
+		if (i != array.length - 1) {
+			urlTail += ' AND ';
+		}
+
+	}
+
+	console.log(urlTail);
+}
+
+function constructUrl(urlParameters, limit) {
+
+	if (typeof limit == 'undefined')
+		limit = 100;
+
+	var url = BASE_URL.concat('$limit=', limit, '&');
+
+	if (urlParameters.length > 0) {
+		formatUrlParameters(urlParameters);
+	} else {
+		return url;
+	}
+
+}
 
 
-
-requestData(BASE_URL, createMenus);
-requestData(BASE_URL, getIncidents);
+requestData(BASE_URL+'$limit=100', createMenus);
+requestData(BASE_URL+'$limit=100', getIncidents);
